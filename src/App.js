630a6df4a9ad0a24877b3602cff3c0315659bf9c
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-03-13 15:51:02 -0500

    added some conditional for different changes to the UI

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import './App.css';
 function App() {
 
   const name = <h1>Matthew Kettelkamp</h1>;
+  const age = 27;
+  const isRed = false;
 
   return (
 
     <div className="App">
       <GetName name="Matthew Kettelkamp" />
+      <GridArea grid1="HTML" grid2="SCSS" grid3="Javascript" grid4="Ruby" grid5="Rails" grid6="React"/>
+
+      <div>
+        {age > 25 ? <h3>You're a badass</h3> : <h3>You're young but almost old</h3>}
+        <h2 style={{color: isRed ? "red" : "black"}}>Get After It</h2>
+        {isRed && <button>Jump In</button>}     {/* adds a button if isRed is true */}
+      </div>
     </div>
   );
 }
@@ -21,5 +30,31 @@ const GetName = (props) => {
   </div>
   );
 };
+
+// Skills Component
+const GridArea = (props) => {
+  return (
+  <div className='skills-container'>
+    <div className='skill'>
+      <p>{props.grid1}</p>
+    </div>
+    <div className='skill'>
+      <p>{props.grid2}</p>
+    </div>
+    <div className='skill'>
+      <p>{props.grid3}</p>
+    </div>
+    <div className='skill'>
+      <p>{props.grid4}</p>
+    </div>
+    <div className='skill'>
+      <p>{props.grid5}</p>
+    </div>
+    <div className='skill'>
+      <p>{props.grid6}</p>
+    </div>
+  </div>
+  )
+}
 
 export default App;
