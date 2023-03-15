a84f5ad59d73a03ffc604683f850bebbccf561f7
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-03-15 16:06:15 -0500

    changed from highlighted green to a strikethrough for the elements that are completed

--- src/Task.js
+++ src/Task.js
@@ -2,7 +2,9 @@
 
 export const Task = (props) => {
   return (
-    <div className='task' style={{ backgroundColor: props.complete ? "green" : "transparent" }}>
+    <div className='task' style={
+      {textDecoration: props.complete ? "line-through" : "none" }
+      }>
       <p className='tick'>⌜</p>
       <p className='taskItem' >{props.taskName}</p>
       <button className='compButt' onClick={() => props.completeTask(props.id)}>✓</button>
