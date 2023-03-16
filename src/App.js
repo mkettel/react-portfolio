0d2b0724bae999cdd430199e511db1e5489e5155
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-03-16 13:33:56 -0500

    added api endpoint for drinks but am changing it to a tldr ai summerizer of text input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Task } from "./Task";
+import Axios  from 'axios';
 
 function App() {
   // STATES
   const [todo, setTodo] = useState([]);  //array for the state of the task list
   const [inputValue, setInputValue] = useState("");
-  const [color, setColor] = useState("black");
+  const [apiInfo, setApiInfo] = useState([]);
+  const [drinkSearch, setDrinkSearch] = useState("");
 
   // FUNCTIONS
   const handleChange = (event) => {
@@ -31,8 +33,39 @@ function App() {
       } else {
         return task;
       }
-    }))
+    }));
   }
+
+  // API PORTION-------------------------------
+  const options = {
+    method: 'GET',
+    url: 'https://the-cocktail-db.p.rapidapi.com/search.php',
+    params: {s: drinkSearch},
+    headers: {
+      'X-RapidAPI-Key': 'ae420f499dmshca2539ee3101f7ap1242b7jsn67b84461ccc8',
+      'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+    }
+  };
+  const fetchDrinkList = () => {
+    if (drinkSearch) {
+      Axios.request(options).then((res) => {
+        setApiInfo(res.data.drinks.map((drink) => {
+          console.log(drink.strDrink);
+          return { strDrink: drink.strDrink };
+        }));
+      }).catch((err) => {
+        console.error(err);
+      })
+    }
+  }
+  useEffect(() => {
+    fetchDrinkList();
+  }, []);
+
+  const drinkChange = (event) => {
+    setDrinkSearch(event.target.value)
+  }
+
   return (
 
     <div className="App">
@@ -47,10 +80,22 @@ function App() {
         {todo.map((task) => {
           return <Task taskName={task.taskName} id={task.id} complete={task.complete} deleteTask={deleteTask} completeTask={completeTask} />
         })}
-
       </div>
-
-
+      {/* API Portion */}
+      <hr />
+      <div className='api-container-input'>
+        <div className='input-section'>
+          <input className='api-input' onChange={drinkChange} />
+          <button className='searchButt' onClick={fetchDrinkList}>Search</button>
+        </div>
+        <h4>DRINK LIST</h4>
+        <div className='drinks'>
+          {apiInfo.map((drink, index) => {
+            return <p key={index}>{drink.strDrink}</p>
+          })}
+          {/* {apiInfo} */}
+        </div>
+      </div>
     </div>
   );
 }
