c7326326f4cb6ee60cd5f88f2e47d034e213da13
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-03-17 16:28:36 -0500

    added react query to the codebase for api calls

--- src/App.js
+++ src/App.js
@@ -7,30 +7,35 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Profile } from './pages/profile';
 import { Navbar } from './navbar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
 export const AppContext = createContext();
 
 
 function App() {
+
+
   // STATES
   const [username, setUsername] = useState("Matt Kettelkamp");
   // const [todo, setTodo] = useState([]);  //array for the state of the task list
-
+  const client = new QueryClient();
 
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ username, setUsername }}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='*' element={<h1>Page Not Found</h1>} />
-        </Routes>
-      </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ username, setUsername }}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='*' element={<h1>Page Not Found</h1>} />
+          </Routes>
+        </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,28 @@
 import { useContext } from "react";
 import { AppContext } from "../App";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export const Home = () => {
   const { username } = useContext(AppContext);
+  const options = {
+    method: 'POST',
+    url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
+    params: {sign: 'sagittarius', day: 'today'},
+    headers: {
+      'X-RapidAPI-Key': 'ae420f499dmshca2539ee3101f7ap1242b7jsn67b84461ccc8',
+      'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
+    }
+  };
+
+  const { data } = useQuery([""], () => {
+    axios.request(options).then((res) => {
+      console.log(res.data);
+    }).catch((err) => {
+      console.log(err);
+    })
+  })
+
   return (
 
   <div className="home-container">
